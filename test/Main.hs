9805c55c8761989061c3b280aba1ca91afa4bea1
Alex Scott <alex@alexscott.net>
2024-08-29 12:52:26 +0200

    Use HUnit to for testing.

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,18 @@
-module Main (main) where
+module Main where
+import Helpers
+import Test.HUnit
+import qualified System.Exit as Exit
+
+testString :: String
+testString = "This is - a self-explained example"
+
+test1 :: Test
+test1 = TestCase (assertEqual "When counting words, consider only the spaced words and exclude any symbols." 5 (length (words(ignoreIrrelevantCharacters testString ))))
+
+tests :: Test
+tests = TestList [TestLabel "Do not count special symbols in the length of a phrase." test1]
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
