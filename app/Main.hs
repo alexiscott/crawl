450484f3a9edf43bdc5fddf65ad3bd5ca8de4880
Alex Scott <alex@alexscott.net>
2024-09-04 01:32:04 +0200

    Use reverse on the two sorts so as to have higher results listed first.

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,39 @@
 module Main where
 
-import Helpers (getCurrentTimestamp,
-                allArticles,
-                lessThanOrEqual5Words,
-                moreThan5Words,
-                httpsClient,
-                sortArticlesByComments,
-                sortArticlesByPoints,
-                printArticles,
-                narrowTags)
+import Helpers
+  ( allArticles
+  , getCurrentTimestamp
+  , httpsClient
+  , lessThanOrEqual5Words
+  , moreThan5Words
+  , narrowTags
+  , printArticles
+  , sortArticlesByComments
+  , sortArticlesByPoints
+  )
 
 main :: IO ()
 main = do
   html <- httpsClient
   let tags = narrowTags html
   currentTimeStamp <- getCurrentTimestamp
-  putStrLn "To see a list of filtered articles with long titles and sorted by the greatest number of comments, enter 1.\nFor shorter headlines and sorted by points, enter 2.\n"
+  putStrLn
+    "To see a list of filtered articles with long titles and sorted by the greatest number of comments, enter 1.\nFor shorter headlines and sorted by points, enter 2.\n"
   result <- getLine
   let userNum = read result :: Int
-  let action | userNum == 1 = putStrLn $ printArticles $ sortArticlesByComments $ moreThan5Words $ allArticles tags
-             | userNum == 2 = putStrLn $ printArticles $ sortArticlesByPoints $ lessThanOrEqual5Words $ allArticles tags
-             | otherwise = putStrLn "Please try again, entering either the number 1 or 2."
+  let action
+        | userNum == 1 =
+          putStrLn .
+          printArticles . reverse . sortArticlesByComments . moreThan5Words $
+          allArticles tags
+        | userNum == 2 =
+          putStrLn .
+          printArticles . reverse . sortArticlesByPoints . lessThanOrEqual5Words $
+          allArticles tags
+        | otherwise =
+          putStrLn "Please try again, entering either the number 1 or 2."
   action
   -- Write the request timestamp to a file.
-  appendFile "scraper-log.txt" $ "The user's filter selection was: " ++ show userNum ++ ", at " ++ currentTimeStamp  ++ "\n"
+  appendFile "scraper-log.txt" $
+    "The user's filter selection was: " ++
+    show userNum ++ ", at " ++ currentTimeStamp ++ "\n"
